7f243ea302a3673029dccdbd6dcdedcd7e0abcdb
Rajib8926 <rajibroy89265@gmail.com>
2025-05-08 20:04:56 +0530

    making heroimage small

--- app/components/HeroImage.tsx
+++ app/components/HeroImage.tsx
@@ -3,16 +3,16 @@ import React from "react";
 import styled from "styled-components";
 
 const ResponsiveImg = styled.img`
-  width: 800px;
+  width: 750px;
 
   @media (max-width: 1200px) {
-    width: 670px;
+    width: 620px;
   }
   @media (max-width: 900px) {
-    width: 550px;
+    width: 510px;
   }
   @media (max-width: 600px) {
-    width: 400px;
+    width: 350px;
   }
 `;
 export default function HeroImage() {
